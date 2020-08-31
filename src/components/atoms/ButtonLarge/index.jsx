f5116a6cbf9fb8a9bf4d44810aa1740c3b48ef87
Francisco Gustavo <hidalgo_fco520@outlook.com>
2020-08-31 01:02:34 -0500

    improvement: component atom/ButtonLarge

---
+++ src/components/atoms/ButtonLarge/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { CSSButton } from './styles';
+
+const ButtonLarge = ({ children }) => (
+  <CSSButton>
+    {children}
+  </CSSButton>
+);
+
+export default ButtonLarge;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
+import ButtonLarge from '../components/atoms/ButtonLarge';
 
 const Container = styled.div`
 text-align: center;
 color: ${props => props.theme.primary}
 `;
 
+
 const Index = () => (
   <Container>
     <h1>Hello World</h1>
+    <ButtonLarge>HOLA</ButtonLarge>
   </Container>
 );
 
