64acba4749c37c909dd564af8820930b65a31db8
Carlos E Navia <cenaviat@gmail.com>
2020-08-31 15:13:03 -0500

    feat component/organisms Login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
-import AvatarUser from '../components/atoms/AvatarUser'
-import { Title } from '../components/atoms/Title'
+import AvatarUser from '../components/atoms/AvatarUser/index';
+import { Title } from '../components/atoms/Title/index';
 
 const IMAGEN = 'https://c.pxhere.com/images/0f/3f/4dbc54d34a6b984a6c5f283be804-1447673.jpg!d'
 
