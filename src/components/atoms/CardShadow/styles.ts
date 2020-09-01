25a9359e0d228ba4a6486723ed679bbf8af310b8
Carlos E Navia <cenaviat@gmail.com>
2020-08-31 19:32:13 -0500

    fix

--- src/components/atoms/CardShadow/styles.ts
+++ src/components/atoms/CardShadow/styles.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export const CSSCard = styled.div`
-border-radius: 10px;
-padding: 20px;
-padding: 60px 70px;
-box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-background-color: #FFF;
+    border-radius: 10px;
+    padding: 20px;
+    padding: 60px 70px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    background-color: #FFF;
 `;
