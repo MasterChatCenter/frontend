3c75c1a0084d8b7c512d8ff3374072391e0bbef7
Francisco Gustavo <hidalgo_fco520@outlook.com>
2020-09-08 23:40:12 -0500

    feat: add login with facebook

--- src/components/molecules/LoginForm/index.tsx
+++ src/components/molecules/LoginForm/index.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
 
+import config from 'root/config';
 import InputText from '@/atoms/InputText';
 import ButtonLarge from '@/atoms/ButtonLarge';
 import ButtonLink from '@/atoms/ButtonLink';
@@ -10,23 +12,42 @@ type props = {
   handleSubmit?: (event: any) => any;
 };
 
-const LoginForm: FC<props> = ({ handleSubmit }) => (
-  <CSSForm onSubmit={handleSubmit}>
-    <InputText
-      name="email"
-      title="Correo electronico:"
-      typeInput="email"
-      placeholder="Escribe tu email"
-    />
-    <InputText
-      name="password"
-      title="Contraseña:"
-      typeInput="password"
-      placeholder="Escribe tu contraseña"
-    />
-    <ButtonLarge type="submit">Iniciar Sesión</ButtonLarge>
-    <ButtonLink>Continuar con facebook</ButtonLink>
-  </CSSForm>
-);
+const LoginForm: FC<props> = ({ handleSubmit }) => {
+  const responseFacebook = (response: any) => {
+    if (response) {
+      return false;
+    }
+  };
+
+  return (
+    <CSSForm onSubmit={handleSubmit}>
+      <InputText
+        name="email"
+        title="Correo electronico:"
+        typeInput="email"
+        placeholder="Escribe tu email"
+      />
+      <InputText
+        name="password"
+        title="Contraseña:"
+        typeInput="password"
+        placeholder="Escribe tu contraseña"
+      />
+      <ButtonLarge type="submit">Iniciar Sesión</ButtonLarge>
+      <FacebookLogin
+        appId={config.appId as string}
+        autoLoad={true}
+        fields="name,email,picture"
+        scope="public_profile,pages_messaging,pages_show_list,pages_manage_metadata"
+        callback={responseFacebook}
+        render={(renderProps: any) => (
+          <ButtonLink handleClick={renderProps.onClick}>
+            Continuar con facebook
+          </ButtonLink>
+        )}
+      />
+    </CSSForm>
+  );
+};
 
 export default LoginForm;
