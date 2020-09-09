5233663b93f33de87fcf8d3f808b263af69b7646
juan sebastian herrera <juan.herreraca@campusucc.edu.co>
2020-09-09 16:36:07 -0500

    feature: panelChat

---
+++ src/components/molecules/PanelChat/styles.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const CSSPanelChat = styled.div`
+  p {
+    padding-left: 10px;
+    font-weight: bold;
+  }
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 375px;
+  height: 65px;
+  border-right: 0px;
+  border-left: 0px;
+  border-bottom: 1px solid #4f4f4f;
+  font-size: 14px;
+  font-family: 'Lato', sans-serif;
+
+  figure {
+    margin-right: 10px;
+  }
+
+  button {
+    border: 0px;
+    background-color: white;
+    outline: none;
+    cursor: pointer;
+  }
+
+  button:last-child {
+    position: relative;
+    bottom: 1px;
+  }
+`;
