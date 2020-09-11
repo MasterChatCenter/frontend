1bc74d239e3a63455d5c598fd1b1b408c28a1dc5
Carlos E Navia <cenaviat@gmail.com>
2020-09-11 15:57:24 -0500

    fix- favicon error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,6 +42,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="shortcut icon" href="../static/favicon.ico" />
         </Head>
         <body>
           <Main />
